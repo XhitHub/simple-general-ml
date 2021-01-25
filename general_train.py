1b818474c16a36d5cf3e4b10033ffb358c725798
XhitHub <com.trojan.exe@gmail.com>
2021-01-25 18:49:40 +0800

    tree eval 2

--- general_train.py
+++ general_train.py
@@ -12,8 +12,17 @@ from sklearn import tree
 #   for item in children:
 #     if (item ==)
 
+SAMPLES_THRESHOLD = 20
+
 def sort_tree_eval(resItem):
-  return resItem['impurity']
+  return resItem['n_node_samples']
+  # return resItem['impurity']
+def filter_tree_eval(resItem):
+  return resItem['n_node_samples'] >= SAMPLES_THRESHOLD
+  # return resItem['impurity']
+
+
+# ref: https://stackoverflow.com/questions/32506951/how-to-explore-a-decision-tree-built-using-scikit-learn
 def eval_decision_tree(tree):
   res = []
   cl = tree.children_left
@@ -22,17 +31,32 @@ def eval_decision_tree(tree):
   i = 0
   # can only look at cl. if node i is leaf, 
   while i < len(cl):
+    # if (True or cl[i] == -1):
     if (cl[i] == -1):
       # is leaf
       resItem = {
+        'i': i,
         'feature': tree.feature[i],
         'n_node_samples': tree.n_node_samples[i],
         'impurity': tree.impurity[i]
       }
-      res.append(resItem)
+      if (resItem['n_node_samples'] >= SAMPLES_THRESHOLD):
+        res.append(resItem)
+    # if (cr[i] == -1):
+    #   # is leaf
+    #   resItem = {
+    #     'i': i,
+    #     'feature': tree.feature[i],
+    #     'n_node_samples': tree.n_node_samples[i],
+    #     'impurity': tree.impurity[i]
+    #   }
+    #   if (resItem['n_node_samples'] >= SAMPLES_THRESHOLD):
+    #     res.append(resItem)
     i += 1
   # sort res
   res.sort(key=sort_tree_eval)
+  # resFiltered = filter(filter_tree_eval, res)
+
   return res
 
 # grid search / RandomizedSearchCV
