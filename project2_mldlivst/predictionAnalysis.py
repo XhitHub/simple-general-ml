868c024d84eacb79e5d9348b3149e1a53645f76b
XhitHub <com.trojan.exe@gmail.com>
2021-03-31 18:43:20 +0800

    making pred analysis

--- project2_mldlivst/predictionAnalysis.py
+++ project2_mldlivst/predictionAnalysis.py
@@ -40,89 +40,19 @@ def run():
         yName = yNames[i]
         predKey = yName + '_predict'
 
-        
-        predProbKey = yName + '_predict_proba_max'
-        # check if the yName's prediction qualifies as picks
-        # print(row)
-        prob = row[predProbKey]
-        # prob = row['0020.XHKG_closeDelta1dR_predict_proba_max']
-        # print(prob)
-        if (prob >= pickProbMin):
-          predFlag = row[predKey][0]
-          predInt = int(row[predKey][1:])
-          # pick rise
-          if (predFlag == 'i' and predInt >= pickMin):
-            ### pass rise
-            # rawPred? No, there won't be rawPred as any pred is already ctg processed classes
-            pickItem = {
-              "item": yName,
-              "prediction": row[predKey],
-              "predInt": predInt,
-              "probability": prob
-            }
-            # actual result, available for old enough records
-            try:
-              actualRow = raw_train_ys_df.loc[rowDateTime]
-              actualRes = actualRow[yName]
-              if (math.isnan(actualRes)):
-                actualRes = 'nan'
-                error = 'nan'
-              else:
-                # to percentage
-                actualRes *= 100
-                error = actualRes - predInt
-                totalRisePicksError += error
-              pickItem['actual_result'] = actualRes
-              pickItem['error'] = error
-            except Exception as e:
-              print('Exception in adding actual result ' + rowDateTime + ', ' + yName)
-              print(traceback.format_exc())
-            tempRisePicks.append(pickItem)
-            risePicksCount += 1
-          # pick drop
-          if (predFlag == 'd' and predInt >= pickMin):
-            ### pass drop
-            # rawPred? No, there won't be rawPred as any pred is already ctg processed classes
-            pickItem = {
-              "item": yName,
-              "prediction": row[predKey],
-              "predInt": predInt,
-              "probability": prob
-            }
-            # actual result, available for old enough records
-            try:
-              actualRow = raw_train_ys_df.loc[rowDateTime]
-              actualRes = actualRow[yName]
-              if (math.isnan(actualRes)):
-                actualRes = 'nan'
-              pickItem['actual_result'] = actualRes
-            except Exception as e:
-              print('Exception in adding actual result ' + rowDateTime + ', ' + yName)
-              print(traceback.format_exc())
-            tempDropPicks.append(pickItem)
-            dropPicksCount += 1
+        predError = row[predKey] - raw_train_ys_df.loc[rowDateTime][yName]
+        item[yName + '_predError'] = predError
       except Exception as e:
-        print('Exception in picking for ' + yName)
-        # print(e)
-        # print(traceback.format_exc())
-    # order the picks
-    item['risePicksCount'] = risePicksCount
-    item['dropPicksCount'] = dropPicksCount
-    if (risePicksCount > 0 and totalRisePicksError != 0):
-      avgRisePicksError = totalRisePicksError / risePicksCount
-      item['avgRisePicksError'] = avgRisePicksError
-      resErrorListItem = {
-        "datetime": rowDateTime,
-        "avgRisePicksError": avgRisePicksError,
-        "risePicksCount": risePicksCount,
-      }
-      resErrorList.append(resErrorListItem)
-    item['risePicks'] = sorted(tempRisePicks, key=lambda item: item["predInt"] + item["probability"], reverse=True)
-    item['dropPicks'] = sorted(tempDropPicks, key=lambda item: item["predInt"] + item["probability"], reverse=True)
+        print('Predict Analysis ' + yName + ' err pt1: ')
+        print(e)
     res.append(item)
   
-  with open(ROOT + '/results/all_predictions_analysis.json', 'w') as fout:
+  with open(ROOT + '/results/all_predictions_analysis_SVR.json', 'w') as fout:
     json.dump(res , fout, indent=2)
+
+  resDf = pd.DataFrame(res)
+  resDf.to_csv(ROOT + '/results/all_predictions_analysis_SVR.csv')
+
 
   resErrorListDf = pd.DataFrame(resErrorList)
   resErrorListDf.to_csv(ROOT + '/results/all_predictions_analysis_avg_errors.csv', index=False, sep="\t")
